ed842f8a0ce047e2e7d7b4c459825008f45c85d8
Pliu07 <jiaxinliu@students.berkeley.net>
2025-08-19 17:50:45 +0000

    done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-document.getElementById("button").addEventListener("click",myFunc);
 
+document.getElementById("button").addEventListener("click",myFunc);
+document.getElementById("button2").addEventListener("click",retrieve);
 function myFunc(){
-    document.getElementById("output").innerHTML = "hi friend!";
+    document.getElementById("output").innerHTML = "information stored";
+    let object = {firstName:"John", lastName:"Doe"};
+    let objectStr = JSON.stringify(object);
+  
+    localStorage.setItem("myItem", objectStr);
 }
+
+function retrieve(){
+    var obj = JSON.parse(localStorage.getItem("myItem"));
+    document.getElementById("output").innerHTML = "information retrieved";
+    console.log(obj)
+}
+
+
+
+
+
 
